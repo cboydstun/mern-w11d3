668c4507fb9cbb697f9bfd94a3a05a7eead71419
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2021-07-14 12:36:58 -0500

    init commit

---
+++ index.js
@@ -0,0 +1,31 @@
+//import dependencies
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const morgan = require('morgan')
+require('dotenv').config();
+
+//initialize express
+const app = express()
+
+//initalize a port
+const SERVER_PORT = process.env.PORT || 5002;
+
+//initalize middleware
+app.use(express.json());
+app.use(cors())
+app.use(morgan(':method :url :response-time'))
+
+//Connect to MongoDB
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+}).then(console.log("Connected to MongoDB")).catch((err)=>{console.log(err)})
+
+//basic SANITY check greeting for deployed API
+app.get('/', (req, res)=>{res.send("Hello Hydrogen Pod!")})
+
+
+//port listening
+app.listen(SERVER_PORT, ()=>{console.log(`Server running at ${SERVER_PORT}`)})
